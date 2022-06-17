ebddc9fe7ae7a4aef74958d0dd85adcc081c68d9
Pengyu (Peter) Liu <79167606+pliumath@users.noreply.github.com>
2022-06-16 17:43:47 -0700

    Add files via upload

--- assets/js/random.js
+++ assets/js/random.js
@@ -1,4 +1,5 @@
-n = 6;
+
+n = 5;
 
 r = Math.floor(Math.random()*n+1);
 
@@ -7,11 +8,9 @@ if(r == 1)
 
 document.write('\
 \
-<h2 class="major">Ronchamp</h2>\
+<h2 class="major">Kelowna</h2>\
 \
 <span class="image main"><img src="images/ran01.jpg" alt="" /></span>\
-\
-<p>This is a photograph of my favorite building on planet earth, Notre-Dame du Haut designed by Le Corbusier. I visited this wonderful chapel right after a nice knot theory workshop at Freiburg im Breisgau. </p>\
 \
 ');
 
@@ -22,7 +21,7 @@ if(r == 2)
 
 document.write('\
 \
-<h2 class="major">München</h2>\
+<h2 class="major">Tofino</h2>\
 \
 <span class="image main"><img src="images/ran02.jpg" alt="" /></span>\
 \
@@ -35,7 +34,7 @@ if(r == 3)
 
 document.write('\
 \
-<h2 class="major">New York City</h2>\
+<h2 class="major">Whistler</h2>\
 \
 <span class="image main"><img src="images/ran03.jpg" alt="" /></span>\
 \
@@ -48,11 +47,9 @@ if(r == 4)
 
 document.write('\
 \
-<h2 class="major">Concert</h2>\
+<h2 class="major">Vancouver</h2>\
 \
 <span class="image main"><img src="images/ran04.jpg" alt="" /></span>\
-\
-<p>I still remember the day I was driving home on I-485 after seeing a friend. The radio was playing, "they say stay in your lane boy, lane boy, but we go where we want to." I stepped on the gas and twenty one pilots became one of my favorite bands. <br><br> "Don\'t trust a perfect person and don\'t trust a song that\'s flawless." </p>\
 \
 ');
 
@@ -63,22 +60,9 @@ if(r == 5)
 
 document.write('\
 \
-<h2 class="major">Vancouver</h2>\
+<h2 class="major">Burnaby</h2>\
 \
 <span class="image main"><img src="images/ran05.jpg" alt="" /></span>\
-\
-');
-
-}
-
-if(r == 6)
-{
-
-document.write('\
-\
-<h2 class="major">Crossword</h2>\
-\
-<span class="image main"><img src="images/ran06.jpg" alt="" /></span>\
 \
 ');
 
